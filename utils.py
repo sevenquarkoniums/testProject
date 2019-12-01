bd7e4d70e77bac8bd2ffa0b9a3ac6aeb91a5c9b8
Jerry Zhang <sevenquarkoniums@gmail.com>
2019-12-01 00:02:38 -0500

    add scc

--- utils.py
+++ utils.py
@@ -42,6 +42,8 @@ def submit(script='name.py', shfile='run.sh', outfile='output.out', system='cori
     if system == 'cori':
         line += 'module load python/3.6-anaconda-5.2\n'
         line += 'export HDF5_USE_FILE_LOCKING=FALSE\n'
+    elif system == 'scc':
+        line += 'module load anaconda3\n'
     line += '%s\n' % script
     fsh.write(line)
     fsh.close()
@@ -51,4 +53,6 @@ def submit(script='name.py', shfile='run.sh', outfile='output.out', system='cori
 
     if system == 'cori':
         command = 'sbatch -N 1 -C haswell --qos=premium -t 9:00:00 -o %s %s' % (outfile, shfile)
+    elif system == 'scc':
+        command = 'qsub -j y -o %s %s' % (outfile, shfile)
     call(command, shell=True)
